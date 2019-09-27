c99ca6b3ace0cbfbf434da923cf40dccfa830297
Mohamed El-Sayed <mohamedesayedmohsen@gmail.com>
2019-09-27 05:05:07 +0200

    Fixing coding styles, testing git ignore.

--- HackAssembler/Header/UtilsHeader/StringUtilities.h
+++ HackAssembler/Header/UtilsHeader/StringUtilities.h
@@ -6,7 +6,7 @@ namespace HackAssembler_Utilities
 	class StringUtilities
 	{
 	public:
-		static std::vector<std::string> & HackAssembler_Utilities::StringUtilities::split(const std::string &s, char delim, std::vector<std::string> &elems);
+		static std::vector<std::string> & split(const std::string &s, char delim, std::vector<std::string> &elems);
 		static bool contains(const std::string & s, char contained);
 		static bool try_Parse_int(const std::string & s, int& output);
 		static bool try_Parse_double(const std::string & s, double & output);
